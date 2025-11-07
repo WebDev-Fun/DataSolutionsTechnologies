2f3af1ae54b41afa0a9d59f25c89c6f946515f66
JUNAID ATTAR <junnaidattar313@gmail.com>
2025-11-07 23:49:46 +0530

    build privacy policy legal page

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  //  where you want to receive messages
+ 
   $to = "info@datasolutionstech.com";
 
-  // collect form data
+
   $name = trim($_POST["name"]);
   $email = trim($_POST["email"]);
   $subject = trim($_POST["subject"]);
   $message = trim($_POST["message"]);
 
-  //  create message body
+
   $fullMessage  = "Name: " . $name . "\n";
   $fullMessage .= "Email: " . $email . "\n";
   $fullMessage .= "Subject: " . $subject . "\n\n";
   $fullMessage .= "Message:\n" . $message . "\n";
 
-  //  headers so the mail looks clean
+
   $headers = "From: " . $email . "\r\n";
   $headers .= "Reply-To: " . $email . "\r\n";
   $headers .= "X-Mailer: PHP/" . phpversion();
 
-  //  send mail
+
   if (mail($to, $subject, $fullMessage, $headers)) {
     echo "Message sent successfully!";
   } else {
